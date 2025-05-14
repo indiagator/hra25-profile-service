12e920908a9e97ff58394990cb8fdfd7b32f6737
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2025-05-14 17:59:26 +0530

    discovery client integrated

--- src/main/java/com/egov/profileservice/MainRestController.java
+++ src/main/java/com/egov/profileservice/MainRestController.java
@@ -3,11 +3,10 @@ package com.egov.profileservice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api/v1")
@@ -20,18 +19,49 @@ public class MainRestController
     CustomerpiiRepository customerpiiRepository;
 
     @Autowired
-    @Qualifier("authValidateWebClient")
-    WebClient authValidateWebClient;
+    ApplicationContext ctx;
+
+//    @Autowired
+//    @Qualifier("authValidateWebClient")
+//    WebClient authValidateWebClient;
+
+    @Autowired
+    TokenService tokenService;
+
+    @GetMapping("getfullname/{userid}")
+    public ResponseEntity<String> getFullname(@PathVariable("userid") Long id,
+                                              @RequestHeader("Authorization") String token
+                                             )
+    {
+
+        log.info("Received request to get fullname for user ID: {}", id);
+
+        if(tokenService.validateToken(token,ctx))
+        {
+             log.info("Valid token: {}", token);
+            return ResponseEntity.ok("dummy name");
+        }
+        else
+        {
+            return ResponseEntity.status(401).body("Invalid token");
+        }
+//        Credential credential = credentialRepository.findById(id).get();
+//        return ResponseEntity.ok(credential.getEmail());
+
+    }
 
     @PostMapping("save/userpii")
     public ResponseEntity<String> saveUserPii(@RequestBody Customerpii customerpii,
-                                                @RequestHeader("Authorization") String token)
+                                                @RequestHeader("Authorization") String token
+                                              )
     {
         // Forward request to Auth-Service for Token Validation
         // If token is valid, update user PII in the database
 
         log.info("Received request to save user PII: {}", customerpii);
         log.info("forwarding request to auth-service for token validation");
+
+        WebClient authValidateWebClient = ctx.getBean("authValidateWebClientEurekaDiscovered", WebClient.class);
 
         String authResponse =authValidateWebClient.get()
                 .header("Authorization", token)
